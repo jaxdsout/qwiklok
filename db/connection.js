45a1898fe55a45395e775519a76a89252b31c777
jaxdsout <140535045+jaxdsout@users.noreply.github.com>
2023-10-17 18:14:06 -0500

    Update connection.js

--- db/connection.js
+++ db/connection.js
@@ -1,35 +1,3 @@
 const mongoose = require('mongoose')
 
-
-const connectionString =
-                  process.env.NODE_ENV === 'production'
-                  ?
-                  process.env.DB_URL
-                  :
-                  'mongodb://localhost/express-books-api';
-
-
-
-mongoose.connect(connectionString, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
-
-
-
-mongoose.connection.on('connected', () => {
-  console.log(`Connected to database ${connectionString}`);
-})
-
-
-mongoose.connection.on('disconnected', () => {
-  console.log(`Disconnected from database ${connectionString}`);
-})
-
-
-mongoose.connection.on('error', (error) => {
-  console.log(`Error connecting to ${connectionString}`);
-  console.error(error);
-})
-
 module.exports = mongoose
