64286e0ace48558849b7f3ed0c425b559899082a
jaxdsout <jaxon.southern@mac.com>
2023-10-17 20:55:33 -0500

    first day of group coding

--- routers/projectRouter.js
+++ routers/projectRouter.js
@@ -1,16 +1,16 @@
 // ROOT IS "http://timepunch.com/project"
 
-const express = require(express);
+const express = require('express');
 const router = express.Router();
 
 router.get('/', allProjects)
 
 router.get('/new-project', newProject)
 
-router.get('/:id', projectDetail)
+// router.get('/:id', projectDetail)
 
-router.put('/:id/edit', editProject)
+// router.put('/:id/edit', editProject)
 
-router.delete('/:id/delete', deleteProject)
+// router.delete('/:id/delete', deleteProject)
 
 module.exports = router
