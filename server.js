ff793ae5085ee2ca0b58a3019c470a79f5697787
jaxdsout <jaxon.southern@mac.com>
2023-10-18 22:20:35 -0500

    day two of group work

--- server.js
+++ server.js
@@ -3,51 +3,51 @@ const express = require('express');
 const app = express();
 const ejsLayouts = require('express-ejs-layouts');
 const methodOverride = require('method-override')
-const { auth, requiresAuth } = require('express-openid-connect');
+// const { auth, requiresAuth } = require('express-openid-connect');
 require('dotenv').config()
 
-const config = {
-  authRequired: false,
-  auth0Logout: true,
-  secret: process.env.SECRET,
-  baseURL: process.env.BASEURL,
-  clientID: process.env.CLIENTID,
-  issuerBaseURL: process.env.ISSUER
-};
+// const config = {
+//   authRequired: false,
+//   auth0Logout: true,
+//   secret: process.env.SECRET,
+//   baseURL: process.env.BASEURL,
+//   clientID: process.env.CLIENTID,
+//   issuerBaseURL: process.env.ISSUER
+// };
 
 // MIDDLEWARE
 app.set('view engine', 'ejs');
 app.use(ejsLayouts);
+app.use(express.static('public'));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(methodOverride("_method"))
 
-app.use(auth(config));
-app.use(jwtCheck);
-
+// app.use(auth(config));
 
 
 // ROUTERS
 
-app.get("/", (req, res) => {
-  console.log(req.oidc.isAuthenticated());
-  res.render("index", { 
-    isAuthenticated: req.oidc.isAuthenticated(),
-    user: req.oidc.user, 
-  })
-})
+// app.get("/", (req, res) => {
+//   console.log(req.oidc.isAuthenticated());
+//   res.render("index", { 
+//     isAuthenticated: req.oidc.isAuthenticated(),
+//     user: req.oidc.user, 
+//   })
+// })
 
-app.get("/secured", requiresAuth(), (req, res) => {
-  console.log(req.oidc.isAuthenticated());
-  res.render("secured", { 
-    isAuthenticated: req.oidc.isAuthenticated(),
-    user: req.oidc.user, 
-  })
-})
+// app.get("/secured", requiresAuth(), (req, res) => {
+//   console.log(req.oidc.isAuthenticated());
+//   res.render("secured", { 
+//     isAuthenticated: req.oidc.isAuthenticated(),
+//     user: req.oidc.user, 
+//   })
+// })
 
-// const userRouter = require("./routers/userRouter")
-// app.use("/users", userRouter);
+const userRouter = require("./routers/userRouter")
+app.use("/user", userRouter);
+
 
 // const projectRouter = require("./routers/projectRouter")
 // app.use("/projects", projectRouter)
