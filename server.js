3d933aa9acd30ed0b0955f21aa434a6fddbe875a
rjsausa <rjsausa@gmail.com>
2023-10-24 14:45:31 -0700

    styling changes

--- server.js
+++ server.js
@@ -2,19 +2,13 @@
 const express = require('express');
 const mongoose = require("mongoose")
 const ejsLayouts = require('express-ejs-layouts');
-<<<<<<< HEAD
-const methodOverride = require('method-override');
-const path = require('path');
-// const { auth, requiresAuth } = require('express-openid-connect');
-require('dotenv').config()
-=======
+
 const methodOverride = require('method-override')
 const cookieParser = require("cookie-parser");
 var bodyParser = require("body-parser");
 require('dotenv').config();
 
 const { DATABASE_URL, PORT } = require("./config");
->>>>>>> groupdev
 
 
 // MIDDLEWARE
@@ -23,11 +17,6 @@ const app = express();
 app.set('view engine', 'ejs');
 app.use(ejsLayouts);
 app.use(express.static('public'));
-<<<<<<< HEAD
-app.use(express.static(path.join(__dirname, 'public')));
-
-=======
->>>>>>> groupdev
 app.use(express.json());
 app.use(bodyParser.json())
 app.use(express.urlencoded({ extended: false }));
