9eeb81eb995fc2d17cc210da1a4fc329ef61e3ac
jaxdsout <jaxon.southern@mac.com>
2023-10-26 11:59:46 -0500

    model work

--- models/user.js
+++ models/user.js
@@ -7,6 +7,10 @@ const UserSchema = new mongoose.Schema({
     startDate: String,
     position: String,
     hourlyPay: Number,
+    admin: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Admin'
+    },
     kloks: [{
         type: mongoose.Schema.Types.ObjectId,
         ref: 'Klok'
